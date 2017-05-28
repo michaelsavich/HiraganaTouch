5bb72ee2247442031dee36f4a0601069199acff5
michaelsavich <savichmichael@icloud.com>
2017-05-28 19:25:19 -0400

    Initial Commit

---
+++ HiraganaTouch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HiraganaTouch
+//
+//  Created by Michael Savich on 5/28/17.
+//  Copyright © 2017 Michael Savich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
